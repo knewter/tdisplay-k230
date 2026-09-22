e1be00f14aaa70001ccf201d85e4c88baf3a4389
Josh Adams <josh@dbadbadba.com>
2026-09-22 14:12:07 -0500

    shell: type desktop catalogue entries

--- nix/touch-launcher/catalog.c
+++ nix/touch-launcher/catalog.c
@@ -7,12 +7,12 @@
 static GDesktopAppInfo *find_id(const char *id) {
   GList *all=g_app_info_get_all(); GDesktopAppInfo *hit=NULL;
   for(GList*l=all;l;l=l->next) { GAppInfo *a=l->data;
-    if(G_IS_DESKTOP_APP_INFO(a) && g_app_info_should_show(a) && !strcmp(g_app_info_get_id(a),id)) { hit=g_object_ref(a); break; }
+    if(G_IS_DESKTOP_APP_INFO(a) && g_app_info_should_show(a) && !strcmp(g_app_info_get_id(a),id)) { hit=G_DESKTOP_APP_INFO(g_object_ref(a)); break; }
   } g_list_free_full(all,g_object_unref); return hit;
 }
 int main(int argc,char**argv) {
  if(argc==1 || (argc==2 && !strcmp(argv[1],"list"))) { GList*all=g_app_info_get_all();
-  for(GList*l=all;l;l=l->next){GAppInfo*a=l->data; if(G_IS_DESKTOP_APP_INFO(a)&&g_app_info_should_show(a)) printf("%s\t%s\t%s\n",g_app_info_get_id(a),g_app_info_get_display_name(a),g_desktop_app_info_get_boolean(G_DESKTOP_APP_INFO(a),"Terminal"));}
+  for(GList*l=all;l;l=l->next){GAppInfo*a=l->data; if(G_IS_DESKTOP_APP_INFO(a)&&g_app_info_should_show(a)) printf("%s\t%s\t%d\n",g_app_info_get_id(a),g_app_info_get_display_name(a),g_desktop_app_info_get_boolean(G_DESKTOP_APP_INFO(a),"Terminal"));}
   g_list_free_full(all,g_object_unref); return 0; }
  if(argc!=3||strcmp(argv[1],"launch")){fprintf(stderr,"usage: k230-desktop-catalog [list|launch ID]\n");return 2;}
  GDesktopAppInfo*a=find_id(argv[2]); if(!a){fprintf(stderr,"desktop entry unavailable: %s\n",argv[2]);return 1;}
